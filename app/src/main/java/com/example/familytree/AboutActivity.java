660b248574aead779772532899b09dbae6785b92
nguyenvietngocquang <nguyenvietngocquang@users.noreply.github.com>
2019-12-07 15:36:31 +0700

    About function

--- app/src/main/java/com/example/familytree/AboutActivity.java
+++ app/src/main/java/com/example/familytree/AboutActivity.java
@@ -3,6 +3,10 @@ package com.example.familytree;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +14,22 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        // Button Cancel
+        Button btn_about_cancel = (Button) findViewById(R.id.btn_about_cancel);
+        btn_about_cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        // Link Facebook
+        TextView tv_facebook = (TextView) findViewById(R.id.tv_facebook);
+        tv_facebook.setMovementMethod(LinkMovementMethod.getInstance());
+
+        // Link GitHub
+        TextView tv_github = (TextView) findViewById(R.id.tv_github);
+        tv_github.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
